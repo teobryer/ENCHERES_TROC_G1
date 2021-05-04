a49a1dd247738b39792e5cb192d6d06e91487889
teobryer <teocalme@gmail.com>
2021-05-04 15:05:20 +0200

    Changement de /home en /accueil

--- src/app/servlet/AccueilServlet.java
+++ src/app/servlet/AccueilServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home")
+@WebServlet("/accueil")
 public class AccueilServlet extends HttpServlet {
 
     @Override
